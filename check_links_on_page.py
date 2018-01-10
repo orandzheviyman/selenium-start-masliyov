b74298094c1700f8a271641ab7bd88644d2de6ea
orandzheviyman <pavel.masliyov@gmail.com>
2018-01-10 14:25:09 +0300

    added requests status and errors indication

--- check_links_on_page.py
+++ check_links_on_page.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+import urllib.request, socket
 
 
 driver = webdriver.Firefox()
@@ -13,7 +14,7 @@ wait = WebDriverWait(driver, 10)
 wait.until(EC.presence_of_element_located((By.NAME,"query")))
 
 #находим количество ссылок
-id = driver.find_elements_by_xpath("|||TARGET_BLOCK|||")
+id = driver.find_elements_by_xpath("//body//div[contains(@class, 'e2-note-list e2-text')]//a")
 n = 0
 
 #проходим по списку
@@ -21,12 +22,25 @@ while n < len(id):
 	#получаем URL ссылки из элемента
 	id = driver.find_elements_by_xpath("|||TARGET_BLOCK|||")
 	links = id[n].get_attribute("href")
-	#выводим URL в консоль
-	print (id[n].get_attribute("href"))
+	
+	#получаем код статуса ответа
+	response = 404
+	try:
+		response = urllib.request.urlopen(links).getcode()
+	except socket.error:
+		print()
+	
+	#выводим URL и статус ответа в консоль
+	if response == 404 :
+		print ("ERROR", response, '   ', id[n].get_attribute("href"))
+	else :
+		print (response, '   ', id[n].get_attribute("href"))
+	
 	#открываем ссылку
 	driver.get(links)
 	n = n + 1
 	driver.back()
+
 print ("Проверили", len(id), "ссылок")
 
 driver.close()

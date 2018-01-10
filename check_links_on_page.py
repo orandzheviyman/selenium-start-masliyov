22fdeea2ac397e54eb67acc98ebfa44d0c1f1df2
orandzheviyman <pavel.masliyov@gmail.com>
2018-01-10 12:45:54 +0300

    Update check_links_on_page.py

--- check_links_on_page.py
+++ check_links_on_page.py
@@ -6,20 +6,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 driver = webdriver.Firefox()
-driver.get("|||targetURL|||")
+driver.get("|||TARGET_URL|||")
 
 #делаем явное ожидание появления элемента
 wait = WebDriverWait(driver, 10)
 wait.until(EC.presence_of_element_located((By.NAME,"query")))
 
 #находим количество ссылок
-id = driver.find_elements_by_xpath("//body//div[contains(@class, 'e2-note-list e2-text')]//a")
+id = driver.find_elements_by_xpath("|||TARGET_BLOCK|||")
 n = 0
 
 #проходим по списку
 while n < len(id):
 	#получаем URL ссылки из элемента
-	id = driver.find_elements_by_xpath("//body//a")
+	id = driver.find_elements_by_xpath("|||TARGET_BLOCK|||")
 	links = id[n].get_attribute("href")
 	#выводим URL в консоль
 	print (id[n].get_attribute("href"))
